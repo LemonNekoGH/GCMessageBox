6b93117c9d526adbc803c82c511232ba562503a5
LemonNeko <chheese048@gmail.com>
2021-08-24 16:25:43 +0800

    第一版发布

--- Game/plugin/MessageBoxCommand.js
+++ Game/plugin/MessageBoxCommand.js
@@ -1,6 +1,115 @@
 var CommandExecute;
 (function (CommandExecute) {
+    function createOnBtnClickFn() {
+        return function (clickToClose, onClick) {
+            if (typeof onClick === 'string') {
+                CommandPage.startTriggerFragmentEvent(onClick, Game.player.sceneObject, Game.player.sceneObject);
+            }
+            else {
+                onClick.run();
+            }
+            if (clickToClose) {
+                GameUI.dispose(1);
+            }
+        };
+    }
+    function setBtnProperties(btn, opt) {
+        btn.label = opt.text;
+        btn.x = opt.x;
+        btn.y = opt.y;
+        btn.width = opt.width;
+        btn.height = opt.height;
+        btn.image1 = opt.normalBg;
+        btn.image2 = opt.hoverBg;
+        btn.image3 = opt.clickBg;
+        btn.grid9img1 = opt.normalGrid;
+        btn.grid9img2 = opt.hoverGrid;
+        btn.grid9img3 = opt.clickGrid;
+        btn.on(EventObject.CLICK, btn, createOnBtnClickFn(), [opt.clickToCloseMessageBox, opt.onClick]);
+    }
+    function showMessageBox(options) {
+        console.log(options);
+        var msgBox = GameUI.get(1);
+        if (msgBox) {
+            return;
+        }
+        GameUI.show(1);
+        msgBox = GameUI.get(1);
+        msgBox.x = options.x;
+        msgBox.y = options.y;
+        msgBox.消息背景.height = options.height;
+        msgBox.消息背景.width = options.width;
+        msgBox.消息文本.height = options.height;
+        msgBox.消息文本.width = options.width;
+        msgBox.消息背景.image = options.background;
+        msgBox.消息文本.text = options.text;
+        var btn = msgBox.确认按钮;
+        var opt = options.confirmBtnOpt;
+        setBtnProperties(btn, opt);
+        btn = msgBox.返回按钮;
+        opt = options.cancelBtnOpt;
+        if (opt) {
+            setBtnProperties(btn, opt);
+        }
+        else {
+            btn.visible = false;
+        }
+    }
+    CommandExecute.showMessageBox = showMessageBox;
     function customCommand_1(commandPage, cmd, trigger, player, playerInput, p) {
+        var btnNormalBg = p.确认按钮正常时背景;
+        var btnNormalGrid = p.确认按钮正常时背景九宫格;
+        var btnHoverBg = p.确认按钮鼠标悬浮时背景;
+        var btnHoverGrid = p.确认按钮鼠标悬浮时背景九宫格;
+        var btnClickBg = p.确认按钮按下时背景;
+        var btnClickGrid = p.确认按钮鼠标按下时背景九宫格;
+        var btnHeight = p.确认按钮高度;
+        var btnWidth = p.确认按钮宽度;
+        var confirmBtnOptions = {
+            x: p.确认按钮位置x,
+            y: p.确认按钮位置y,
+            width: btnWidth,
+            height: btnHeight,
+            text: p.确认按钮文本,
+            normalBg: btnNormalBg,
+            normalGrid: btnNormalGrid,
+            hoverBg: btnHoverBg,
+            hoverGrid: btnHoverGrid,
+            clickBg: btnClickBg,
+            clickGrid: btnClickGrid,
+            onClick: p.当确认时执行,
+            clickToCloseMessageBox: p.确认按钮点击后关闭对话框
+        };
+        var cancelBtnOptions = undefined;
+        if (p.对话框类型 === 1) {
+            var useSameSize = p.取消按钮与确认按钮使用相同高宽;
+            var useSameBgAndGrid = p.取消按钮与确认按钮使用相同的背景和九宫格;
+            cancelBtnOptions = {
+                x: p.取消按钮位置x,
+                y: p.取消按钮位置y,
+                width: useSameSize ? btnWidth : p.取消按钮宽度,
+                height: useSameSize ? btnHeight : p.取消按钮高度,
+                text: p.取消按钮文本,
+                normalBg: useSameBgAndGrid ? btnNormalBg : p.取消按钮正常时背景,
+                normalGrid: useSameBgAndGrid ? btnNormalGrid : p.取消按钮正常时背景九宫格,
+                hoverBg: useSameBgAndGrid ? btnHoverBg : p.取消按钮鼠标悬浮时背景,
+                hoverGrid: useSameBgAndGrid ? btnHoverGrid : p.取消按钮鼠标悬浮时背景九宫格,
+                clickBg: useSameBgAndGrid ? btnClickBg : p.取消按钮按下时背景,
+                clickGrid: useSameBgAndGrid ? btnClickGrid : p.取消按钮鼠标按下时背景九宫格,
+                onClick: p.当取消时执行,
+                clickToCloseMessageBox: p.取消按钮点击后关闭对话框
+            };
+        }
+        showMessageBox({
+            x: p.对话框本体位置x,
+            y: p.对话框本体位置y,
+            text: p.消息文本,
+            width: p.对话框本体宽度,
+            height: p.对话框本体高度,
+            background: p.对话框本体背景,
+            confirmBtnOpt: confirmBtnOptions,
+            cancelBtnOpt: cancelBtnOptions
+        });
     }
     CommandExecute.customCommand_1 = customCommand_1;
 })(CommandExecute || (CommandExecute = {}));
--- Game/system/UIRuntime.ts
+++ Game/system/UIRuntime.ts
@@ -6,11 +6,10 @@
  * 1-对话框界面 [BASE]
  */
 class GUI_1 extends GUI_BASE {
-   对话框容器:UIRoot;
    消息背景:UIBitmap;
-   消息文本:UIString;
    确认按钮:UIButton;
    返回按钮:UIButton;
+   消息文本:UIString;
    constructor(){
       super(1);
    }
@@ -18,7 +17,6 @@ class GUI_1 extends GUI_BASE {
 class ListItem_1 extends UIListItemData {
    消息背景:string;
    消息文本:string;
-
 }
 
 /**
@@ -27,13 +25,18 @@ class ListItem_1 extends UIListItemData {
 class GUI_1001 extends GUI_BASE {
    标题:UIString;
    显示确认框按钮:UIButton;
-   显示对话框按钮:UIButton;
+   显示提示框按钮:UIButton;
+   当确认框确认时出现:UIString;
+   当确认框取消时出现:UIString;
+   当提示框确认时出现:UIString;
    constructor(){
       super(1001);
    }
 }
 class ListItem_1001 extends UIListItemData {
    标题:string;
-
+   当确认框确认时出现:string;
+   当确认框取消时出现:string;
+   当提示框确认时出现:string;
 }
 GameUI["__compCustomAttributes"] = {"UIRoot":["enabledLimitView","scrollShowType","hScrollBar","hScrollBg","vScrollBar","vScrollBg","scrollWidth","slowmotionType","enabledWheel","hScrollValue","vScrollValue"],"UIButton":["label","image1","grid9img1","image2","grid9img2","image3","grid9img3","fontSize","color","overColor","clickColor","bold","italic","smooth","align","valign","letterSpacing","font","textDx","textDy","textStroke","textStrokeColor"],"UIBitmap":["image","grid9","flip","pivotType"],"UIString":["text","fontSize","color","bold","italic","smooth","align","valign","leading","letterSpacing","font","wordWrap","overflow","shadowEnabled","shadowColor","shadowDx","shadowDy","stroke","strokeColor","onChangeFragEvent"],"UIVariable":["varID","fontSize","color","bold","italic","smooth","align","valign","leading","letterSpacing","font","wordWrap","overflow","shadowEnabled","shadowColor","shadowDx","shadowDy","stroke","strokeColor","onChangeFragEvent"],"UICustomGameNumber":["customData","previewNum","previewFixed","fontSize","color","bold","italic","smooth","align","valign","leading","letterSpacing","font","wordWrap","overflow","shadowEnabled","shadowColor","shadowDx","shadowDy","stroke","strokeColor"],"UICustomGameString":["customData","inEditorText","fontSize","color","bold","italic","smooth","align","valign","leading","letterSpacing","font","wordWrap","overflow","shadowEnabled","shadowColor","shadowDx","shadowDy","stroke","strokeColor"],"UIAvatar":["avatarID","scaleNumberX","scaleNumberY","orientationIndex","avatarFPS","playOnce","isPlay","avatarFrame","actionID","avatarHue"],"UIStandAvatar":["avatarID","actionID","scaleNumberX","scaleNumberY","flip","playOnce","isPlay","avatarFrame","avatarFPS","avatarHue"],"UIAnimation":["animationID","scaleNumberX","scaleNumberY","aniFrame","playFps","playType","showHitEffect","silentMode"],"UIInput":["text","fontSize","color","bold","italic","align","leading","font","wordWrap","restrict","inputMode","maxChars","shadowEnabled","shadowColor","shadowDx","shadowDy","onInputFragEvent","onEnterFragEvent"],"UICheckBox":["selected","image1","grid9img1","image2","grid9img2","onChangeFragEvent"],"UISwitch":["selected","image1","grid9img1","image2","grid9img2","previewselected","onChangeFragEvent"],"UITabBox":["selectedIndex","itemImage1","grid9img1","itemImage2","grid9img2","itemWidth","itemHeight","items","rowMode","spacing","labelSize","labelColor","labelFont","labelBold","labelItalic","smooth","labelAlign","labelValign","labelLetterSpacing","labelSelectedColor","labelDx","labelDy","labelStroke","labelStrokeColor","onChangeFragEvent"],"UISlider":["image1","bgGrid9","image2","blockGrid9","image3","blockFillGrid9","step","min","max","value","transverseMode","blockFillMode","blockPosMode","fillStrething","onChangeFragEvent"],"UIGUI":["guiID","instanceClassName","dpWidth"],"UIList":["itemModelGUI","previewSize","selectEnable","repeatX","itemWidth","itemHeight","spaceX","spaceY","scrollShowType","hScrollBar","hScrollBg","vScrollBar","vScrollBg","scrollWidth","selectImageURL","selectImageGrid9","selectedImageAlpha","selectedImageOnTop","overImageURL","overImageGrid9","overImageAlpha","overImageOnTop","overSelectMode","slowmotionType"],"UIComboBox":["itemLabels","selectedIndex","bgSkin","bgGrid9","fontSize","color","bold","italic","smooth","align","valign","letterSpacing","font","textDx","textStroke","textStrokeColor","displayItemSize","listScrollBg","listScrollBar","listAlpha","itemHeight","listBgColor","itemFontSize","itemColor","itemBold","itemItalic","itemAlign","itemValign","itemLetterSpacing","itemFont","itemOverColor","itemOverBgColor","itemTextDx","itemTextDy","itemTextStroke","itemTextStrokeColor","onChangeFragEvent"],"UIVideo":["videoURL","playType","volume","playbackRate","currentTime","muted","loop","pivotType","flip","onLoadedFragEvent","onErrorFragEvent","onCompleteFragEvent"]};
